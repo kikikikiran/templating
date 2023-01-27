318cd0dd7d9da72b4639e1d9c9455013d8a968ff
kikikikiran <108289287+kikikikiran@users.noreply.github.com>
2023-01-27 13:22:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,9 @@ template "aws_ec2_instance" {
 module "example_instance" {
   source = "./templates/aws_ec2_instance.tpl"
 
-  ami           = "ami-0aa7d40eeae50c9a9"
-  instance_type = "t2.micro"
-  key_name      = "hpe"
-  subnet_id     = "subnet-02f92f936462845ec"
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
 }
+
