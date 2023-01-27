03933e9b62460bd9cdc4f5cf0284a72be145f7a5
kikikikiran <108289287+kikikikiran@users.noreply.github.com>
2023-01-27 13:30:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-template "aws_ec2_instance" {
-  source = "./templates/aws_ec2_instance.tpl"
-
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = var.subnet_id
-}
 
 module "example_instance" {
   source = "./templates/aws_ec2_instance.tpl"
