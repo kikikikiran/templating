dd982050b0b8f4bc69ffd1b9c7d3d010ebe15322
kikikikiran <108289287+kikikikiran@users.noreply.github.com>
2023-01-27 12:47:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+template "aws_ec2_instance" {
+  source = "./templates/aws_ec2_instance.tpl"
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+}
+
+module "example_instance" {
+  source = "template.aws_ec2_instance"
+
+  ami           = "ami-0aa7d40eeae50c9a9"
+  instance_type = "t2.micro"
+  key_name      = "hpe"
+  subnet_id     = "subnet-02f92f936462845ec"
+}
