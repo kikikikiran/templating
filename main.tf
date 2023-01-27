52fe6765747bd972653d5cdbc5fb8d037d4ed03a
kikikikiran <108289287+kikikikiran@users.noreply.github.com>
2023-01-27 13:10:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ template "aws_ec2_instance" {
 }
 
 module "aws_instance" {
-  source = "template.aws_ec2_instance"
+  source = "template_file.aws_ec2_instance.tpl"
 
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
